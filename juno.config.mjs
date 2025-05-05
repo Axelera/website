42f3452d390604cb8e841a8c188e48a7efa07d9a
ilbertt <luca.berte98@hotmail.it>
2025-05-06 00:08:58 +0200

    feat: juno analytics

--- juno.config.mjs
+++ juno.config.mjs
@@ -5,5 +5,8 @@ export default defineConfig({
   satellite: {
     id: 'sg4ml-jiaaa-aaaal-ascma-cai',
     source: 'public',
-  }
+  },
+  orbiter: {
+    id: 'ugrbl-7aaaa-aaaas-amlqq-cai',
+  },
 });
